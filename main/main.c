cc0b18bfec6b2134caa8f9f62deb2e939db42e0f
Viggo Petersen <vig56@hotmail.com>
2022-06-16 22:34:18 +0200

    Add benchmarks
    Add benchmarks for Chacha20 and Poly1305

--- main/main.c
+++ main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -10,7 +11,10 @@
 #include "test_chacha.h"
 #include "test_poly.h"
 
-void test_chacha20_poly1305(const char *text_key, const char *text_nonce, const char *text_plain, const char *text_cipher, uint64_t counter, unsigned int number) {
+/**
+ * @brief Combined test of Chacha20 and Poly1305
+ */
+void test_chacha20_poly1305(const char *text_key, const char *text_nonce, const char *text_plain, uint64_t counter) {
   printf("Testing full chacha20-poly1305\n");
 
   // Chacha20 setup
@@ -19,12 +23,10 @@ void test_chacha20_poly1305(const char *text_key, const char *text_nonce, const 
   uint8_t nonce[8];
   uint32_t len = strlen(text_plain) / 2;
   uint8_t *plain = alloca(len);
-  uint8_t *cipher = alloca(len);
   uint8_t *output = alloca(len);
   hex2byte(text_key, chacha_key);
   hex2byte(text_nonce, nonce);
   hex2byte(text_plain, plain);
-  hex2byte(text_cipher, cipher);
   chacha20_setup(&ctx, chacha_key, sizeof(chacha_key), nonce);
 
   //Exact length test
@@ -52,13 +54,72 @@ void test_chacha20_poly1305(const char *text_key, const char *text_nonce, const 
   printf("CHACHA20-POLY1305 test succeeded!\n");
 }
 
+/**
+ * @brief Benchmark of Chacha20 algorithm
+ */
+void benchmark_chacha(const char *text_key, const char *text_nonce, const char *text_plain) {
+  chacha20_ctx ctx;
+  uint8_t chacha_key[32];
+  uint8_t nonce[8];
+  uint32_t len = strlen(text_plain) / 2;
+  uint8_t *plain = alloca(len);
+  uint8_t *output = alloca(len);
+  hex2byte(text_key, chacha_key);
+  hex2byte(text_nonce, nonce);
+  hex2byte(text_plain, plain);
+  chacha20_setup(&ctx, chacha_key, sizeof(chacha_key), nonce);
+  memset(output, 0, len);
+  // benchmark encryption
+  printf("Starting chacha benchmark\n");
+  for (int j = 0; j < 10; j++) {
+    clock_t t = clock();
+    for (int i = 0; i < 1000; i++) {
+      chacha20_encrypt_bytes(&ctx, plain, output, len);
+    }
+    t = clock() - t;
+    double time_spent = (double)t / CLOCKS_PER_SEC;
+    printf("Took %fs\n", time_spent);
+  }
+}
+
+/**
+ * @brief Benchmark of Poly1305 algorithm
+ */
+void benchmark_poly(char *text) {
+	unsigned char key[32];
+	unsigned char mac[16];
+  unsigned char msg[1001];
+	size_t i;
+	poly1305_power_on_self_test();
+	for (i = 0; i < sizeof(key); i++)
+		key[i] = (unsigned char)(i + 221);
+  for (i = 0; i < sizeof(msg); i++)
+		msg[i] = (unsigned char)text[i];
+
+  // benchmark encryption
+  printf("Starting poly benchmark\n");
+  for (int j = 0; j < 10; j++) {
+    clock_t t = clock();
+    for (int i = 0; i < 1000; i++) {
+      poly1305_auth(mac, msg, sizeof(msg), key);
+    }
+    t = clock() - t;
+    double time_spent = (double)t / CLOCKS_PER_SEC;
+    printf("Took %fs\n", time_spent);
+  }
+}
 
 void app_main(void) {
   printf("Welcome!\n");
   srand(0);
+  int l = 1000;
+  char *str = malloc(l + 1);
+  memset(str, 'A', l);
   test_chacha_run();
   test_poly1305();
-  test_chacha20_poly1305("0000000000000000000000000000000000000000000000000000000000000000", "0000000000000000", "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", "76b8e0ada0f13d90405d6ae55386bd28bdd219b8a08ded1aa836efcc8b770dc7da41597c5157488d7724e03fb8d84a376a43b8f41518a11cc387b669b2ee6586", 0, 1);
+  test_chacha20_poly1305("0000000000000000000000000000000000000000000000000000000000000000", "0000000000000000", "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", 0);
+  benchmark_chacha("0000000000000000000000000000000000000000000000000000000000000000", "0000000000000000", str);
+  benchmark_poly(str);
   for (int i = 10; i >= 0; i--) {
     printf("Restarting in %d seconds...\n", i);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
